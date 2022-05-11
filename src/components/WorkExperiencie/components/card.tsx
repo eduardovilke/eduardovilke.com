f18b0bbed88557b09afc5101dc1e246cc8cc9658
Eduardo Vilke <eduardo.svilke@gmail.com>
2022-05-11 20:34:03 -0300

    fix key prop card component

--- src/components/WorkExperiencie/components/card.tsx
+++ src/components/WorkExperiencie/components/card.tsx
@@ -1,12 +1,11 @@
 interface CardProps {
-  key: string;
   title: string;
   description: string;
 }
 
-export function Card ({ key, title, description }: CardProps): JSX.Element {
+export function Card ({ title, description }: CardProps): JSX.Element {
   return (
-    <div key={key} className="card mb-6">
+    <div className="card mb-6">
       <div className="flex flex-col bg-gray rounded-xl p-8">
         <h4 className="font-bold bg-clip-text bg-gradient-to-r from-red via-blue to-blue text-transparent">EXPERIÊNCIA PROFISSIONAL</h4>
         <h3 className="font-bold text-white py-4 text-3xl	">
--- src/components/WorkExperiencie/index.tsx
+++ src/components/WorkExperiencie/index.tsx
@@ -3,14 +3,17 @@ import { Card } from "./components/card";
 export function WorkExperience(){
   const jobs = [
     {
+      id: 1,
       title: 'ZRP',
       description: 'Desenvolvimento de soluções utilizando NodeJS, Serverless, AWS Lambda, VueJS, React, Ruby on Rails.'
     },
     {
+      id: 2,
       title: 'Twoweb Digital',
       description: 'Desenvolvimento de layouts de e-commerce, integrações via API’s, desenvolvimento de websites e aplicativos mobile. Utilizei as tecnologias: Ruby On Rails, Sass, React, Redux, React Native, NodeJS, AdonisJS e Docker.'
     },
     {
+      id: 3,
       title: 'Online Sistemas',
       description: 'Suporte de sistemas ERP e desenvolvimento do sistema em nuvem, utilizando tecnologias como HTML, CSS, Javascript, SQL e PHP.'
     }
@@ -19,7 +22,7 @@ export function WorkExperience(){
     <div className="flex flex-col">
       {jobs.map(job => (
         <Card 
-          key={job.title} 
+          key={job.id}
           title={job.title} 
           description={job.description}
         />
