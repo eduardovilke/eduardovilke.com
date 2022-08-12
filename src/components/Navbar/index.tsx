66cad28fd09bae898f81aa15fb5f6abf83751743
Eduardo Vilke <eduardo.svilke@gmail.com>
2022-08-12 00:27:38 -0300

    feat: change icon toggle theme

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,4 @@
-import { FaSun, FaMoon} from 'react-icons/fa';
+import { BsSunFill, BsMoonFill } from 'react-icons/bs';
 import { useTheme } from '../../hooks/useTheme';
 
 export const Navbar = (): JSX.Element => {
@@ -13,7 +13,7 @@ export const Navbar = (): JSX.Element => {
         className='flex items-center justify-center mb-4 border border-transparent text-base font-medium rounded-xl dark:text-gray-light text-gray-300 bg-white dark:bg-gray hover:bg-white dark:hover:bg-gray-200 py-2 text-lg px-2'
         onClick={toggleTheme}
       >
-        {theme === 'dark' ? <FaMoon /> : <FaSun />}
+        {theme === 'dark' ? <BsMoonFill /> : <BsSunFill />}
       </button>
     </div>
   )
