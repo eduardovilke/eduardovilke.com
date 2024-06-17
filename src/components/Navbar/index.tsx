0938b136975becb72e690093f37b84ec1082e3d3
Eduardo Vilke <eduardo.svilke@gmail.com>
2024-06-17 19:17:59 -0300

    feat: added switcher lang

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,6 @@
 import { BsSunFill, BsMoonFill } from 'react-icons/bs'
 import { useTheme } from '@hooks/useTheme'
+import { useRouter } from 'next/router'
 
 export const Navbar = (): JSX.Element => {
 
@@ -7,13 +8,22 @@ export const Navbar = (): JSX.Element => {
 
   const toggleTheme = (): void => toggle()
 
+  const router = useRouter()
+  const toggleLang = () => router.push({}, undefined, { locale: router.locale === 'pt-BR' ? 'en-US' : 'pt-BR'})
+
   return (
-    <div className='flex justify-end'>
+    <div className='flex justify-end mb-4'>
       <button
-        className='flex items-center justify-center mb-4 border border-transparent text-base font-medium rounded-xl dark:text-gray-light text-gray-300 bg-white dark:bg-gray hover:bg-white dark:hover:bg-gray-200 py-2 text-lg px-2 ease-in duration-200'
+        className='flex items-center justify-center mr-2 border border-transparent font-medium rounded-xl dark:text-gray-light text-gray-300 bg-white dark:bg-gray hover:bg-white dark:hover:bg-gray-200 py-2 px-4 ease-in duration-100'
+        onClick={toggleLang}
+      >
+        {router.locale === 'en-US' ? 'PT-BR' : 'EN-US'}
+      </button>
+      <button
+        className='flex items-center justify-center border border-transparent font-medium rounded-xl dark:text-gray-light text-gray-300 bg-white dark:bg-gray hover:bg-white dark:hover:bg-gray-200 py-2 text-lg px-4 ease-in duration-200'
         onClick={toggleTheme}
       >
-        {theme === 'dark' ? <BsMoonFill /> : <BsSunFill />}
+        {theme === 'dark' ? <BsSunFill /> : <BsMoonFill />}
       </button>
     </div>
   )
