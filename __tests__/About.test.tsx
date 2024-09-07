9de446425dcf06ff3b7617883d7a0ba9fd1fca1c
Eduardo Vilke <eduardo.svilke@gmail.com>
2024-09-07 11:38:40 -0300

    test: fixed about

--- __tests__/About.test.tsx
+++ __tests__/About.test.tsx
@@ -1,9 +1,21 @@
 import { expect, test } from 'vitest'
 import { render } from '@testing-library/react'
 import { About } from '../src/components/About'
+import { NextIntlClientProvider } from 'next-intl'
+import { ThemeProvider } from '../src/hooks/useTheme'
 
 test('About component', async () => {
-  const { findAllByText } = render(<About />)
+  const { findAllByText } = render(
+    <NextIntlClientProvider 
+      locale="pt-BR"
+      timeZone="America/Sao_Paulo"
+      messages={ (await import(`../src/dictionaries/pt-BR.json`)).default}
+    >
+      <ThemeProvider>
+        <About />
+      </ThemeProvider>
+    </NextIntlClientProvider>
+  )
   
   expect(findAllByText('Eduardo Vilke')).resolves.toBeDefined()
 })
